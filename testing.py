e6999ead2b232ee98a5548589be0508b2285c92d
Jayden Clark <jayden260100@gmail.com>
2021-08-03 11:40:29 +1200

    init commit

---
+++ testing.py
@@ -0,0 +1,15 @@
+# Testing script for modules
+
+# Step 1: Generate invalid and valid input values
+
+#primary_keys = {
+#   1:{"valid":True, "value":1}
+#}
+
+movie_name = {
+    
+}
+
+# Generate valid
+for i in range(0,30):
+    pass
