78c5d1ef5cac81529026814cfabe0e8e243795c0
Jayden Clark <jayden260100@gmail.com>
2021-08-03 12:26:43 +1200

    invalid & valid

--- testing.py
+++ testing.py
@@ -1,15 +1,88 @@
-# Testing script for modules
+""" Testing script for modules
 
-# Step 1: Generate invalid and valid input values
+Step 1: Generate invalid and valid input values for all data inputs
 
-#primary_keys = {
-#   1:{"valid":True, "value":1}
-#}
+Primary Keys, movie name, year of release, rating, runtime, genre
 
-movie_name = {
-    
+primary_keys = {
+   1:{"valid":True, "value":1}
+}
+"""
+
+import random, string
+
+data_inputs = {
+    "primary_key":{
+        "valid":[],
+        "invalid":[]
+    },
+    "movie_name":{
+        "valid":[],
+        "invalid":[]
+    },
+    "year_of_release":{
+        "valid":[],
+        "invalid":[]
+    },
+    "rating":{
+        "valid":[],
+        "invalid":[]
+    },
+    "runtime":{
+        "valid":[],
+        "invalid":[]
+    },
+    "genre":{
+        "valid":[],
+        "invalid":[]
+    }
 }
 
-# Generate valid
-for i in range(0,30):
-    pass
+def generate_valid(type, max_length=50, max_value=10000, min_value=0):
+    if(type == "str"):
+        text = ''.join(random.choices(string.ascii_letters, k=max_length))
+        return text
+    if(type == "int"):
+        n = random.randint(min_value,max_value)
+        return n
+
+def generate_invalid(type, max_length=50, max_value=10000, min_value=0):
+    if(type == "str"):
+        flt = random.uniform(min_value, max_value)
+        n = random.randint(min_value,max_value)
+        emp = ""
+        chosen = random.randint(0,2)
+        if(chosen == 0):
+            return flt 
+        elif(chosen == 1):
+            return n 
+        else:
+            return emp
+    if(type == "int"):
+        negative = random.randint(-max_value,min_value)
+        flt = random.uniform(min_value, max_value)
+        emp = ""
+        text = ''.join(random.choices(string.ascii_letters, k=max_length))
+        chosen = random.randint(0,3)
+        if(chosen == 0):
+            return negative 
+        elif(chosen == 1):
+            return flt 
+        elif(chosen == 2):
+            return emp
+        else:
+            return text
+
+
+print(generate_invalid("int"))
+
+# Generate valid & invalids
+for input, values in data_inputs.items():
+    if(input == "primary_key"):
+        # Valid inputs
+        for i in range(0, 30):
+            pass
+
+
+
+    
