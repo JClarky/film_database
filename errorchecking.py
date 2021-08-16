d6d1b1935363372910aba2185726cf4a06df7c42
Jayden Clark <jayden260100@gmail.com>
2021-08-17 09:06:15 +1200

    Errorchecking outline

--- errorchecking.py
+++ errorchecking.py
@@ -1,4 +1,17 @@
 import database_control as dbc
 
 def film(pk):
-    # Return
+    # Return film
+    pass
+
+def insert():
+    # Insert film into database
+    pass
+
+def amend():
+    # Amend film into database
+    pass
+
+def delete(pk):
+    # Delete film from database
+    pass
