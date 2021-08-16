c81d37af0a0edd014e9ecc6b4ee9b585977d237c
Jayden Clark <jayden260100@gmail.com>
2021-08-17 09:38:44 +1200

    Error checking module, film function

--- errorchecking.py
+++ errorchecking.py
@@ -2,7 +2,13 @@ import database_control as dbc
 
 def film(pk):
     # Return film
-    pass
+    returned = dbc.film(pk)
+    if(type(returned) == type(())):
+        # Success
+        return(returned, True, None)
+    else:
+        # Failure
+        return(returned, False, "Film does not exist...")
 
 def insert():
     # Insert film into database
