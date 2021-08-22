a1b6598c7e20a069cd9667b65a4530ba53d7df32
Jayden Clark <jayden260100@gmail.com>
2021-08-22 13:01:45 +1200

    Testing script completed for errorchecking module

--- testing.py
+++ testing.py
@@ -11,6 +11,7 @@ primary_keys = {
 
 import random, string
 import database_control as dbc
+import errorchecking as ec
 
 last_added_pk = None
 
@@ -177,6 +178,121 @@ def database_control_test():
 
     return(True, "Passed all tests")
 
+def error_checking_test():
+    global last_added_pk
+    whole_db = dbc.whole_db()
+    
+    # Check entire module on valid values first
+    # Then, if passed, select data entry to be invalid (cycle through)
+    # Test Entire module 30 times
+    
+    #
+    # VALID DATA CHECK
+    #
+
+    # Repeat 30 times
+    for i in range(0,30):
+        # Check single film
+        preselected = whole_db[random.randint(0,len(whole_db)-1)]
+        returned = ec.film(preselected["PRIMARY_KEY"]) # Request first film
+        # Compare the returned film, and the preselected film
+        if returned == "":
+            return(False, "Empty film returned")
+        elif type(returned) != type({}):
+            return(False, "Film returned not in dictionary format, returned in "+str(type(returned))+" returned: "+str(returned))
+        elif preselected != returned:
+            return(False, str("Film returned not the same as the requested film preselected: "+str(preselected)+" returned: "+str(returned)))
+
+        # Add movie
+        idx = random.randint(0,29)
+        pk = len(dbc.whole_db())+1
+        last_added_pk = pk
+        name = data_inputs["primary_key"]["valid"][idx]
+        yor = data_inputs["year_of_release"]["valid"][idx]
+        rating = data_inputs["rating"]["valid"][idx]
+        runtime = data_inputs["runtime"]["valid"][idx]
+        genre = data_inputs["genre"]["valid"][idx]
+        inserted_dict = {'PRIMARY_KEY': pk, 'MOVIE_NAME': str(name), 'YEAR_OF_RELEASE': yor, 'RATING': rating, 'RUNTIME': runtime, 'GENRE': genre}
+
+        ec.insert(pk,  name, yor, rating, runtime, genre)
+        inserted = ec.film(pk)
+        if inserted == "":
+            return(False, "Empty inserted film returned")
+        elif str(inserted) != str(inserted_dict):
+            return(False, "Film returned not matching inserted film")
+
+        # Amend movie
+        # Ensure the new value is actually different then the last value
+        double_up = True
+        while double_up:
+            idx = random.randint(0,29)
+            name = data_inputs["primary_key"]["valid"][idx]
+            yor = data_inputs["year_of_release"]["valid"][idx]
+            rating = data_inputs["rating"]["valid"][idx]
+            runtime = data_inputs["runtime"]["valid"][idx]
+            genre = data_inputs["genre"]["valid"][idx]
+            new_random = [{'name':"PRIMARY_KEY", 'value': pk}, {'name':"MOVIE_NAME", 'value': str(name)}, {'name':"YEAR_OF_RELEASE", 'value': yor}, {'name':"RATING", 'value': rating}, {'name':"RUNTIME", 'value': runtime}, {'name':"GENRE", 'value': genre}]
+            
+            selected_to_change = random.randint(1,5)
+            if inserted_dict[new_random[selected_to_change]["name"]] != new_random[selected_to_change]["value"]:
+                break
+
+
+        ec.amend(pk, new_random[selected_to_change]["name"], new_random[selected_to_change]["value"])
+        amended = ec.film(pk)
+        if amended == "":
+            return(False, "Empty amended film returned")
+        elif amended == inserted:
+            return(False, "Film returned not matching amended film, amended="+str(new_random)+" returned= "+str(amended))
+
+        # Delete movie
+        ec.delete(pk)
+        if(type(ec.film(pk)) == type({})):
+            return(False, "Film has not been succesfully deleted")
+
+    #
+    # INVALID DATA CHECK
+    #
+    
+    # Cycle through all data inputs
+    for key, value in data_inputs.items():
+        # Test 30 times
+        for i in range(0,30):
+            created_values = {'primary_key': data_inputs["primary_key"]["valid"][random.randint(0,29)], 
+                            'movie_name': data_inputs["movie_name"]["valid"][random.randint(0,29)], 
+                            'year_of_release': data_inputs["year_of_release"]["valid"][random.randint(0,29)], 
+                            'rating': data_inputs["rating"]["valid"][random.randint(0,29)], 
+                            'runtime': data_inputs["runtime"]["valid"][random.randint(0,29)], 
+                            'genre': data_inputs["genre"]["valid"][random.randint(0,29)]}
+
+            created_values[key] = data_inputs[key]["invalid"][random.randint(0,29)]
+            
+            # Check single film
+            if(key == "pk"):
+                returned, flag, reason = ec.film(created_values["primary_key"])
+                if(flag):
+                    return(False, "Film has been returned, but primary_key was invalid")
+            
+            # Check deleting film
+            if(key == "pk"):
+                selected_invalid_idx = random.randint(0,29)
+                returned, flag, reason = ec.delete(created_values["primary_key"])
+                if(flag):
+                    return(False, "Film has been deleted, but primary_key was invalid")
+            
+            # Check add film
+            returned, flag, reason = ec.insert(created_values["primary_key"], created_values["movie_name"], created_values["year_of_release"], created_values["rating"], created_values["runtime"], created_values["genre"])
+            if(flag):
+                return(False, "Inserted film to database with invalid value for "+key+" reason was: "+reason)
+
+            # Check inseting film
+            returned, flag, reason = ec.amend(created_values["primary_key"], key.upper(), created_values[key])
+            if(flag):
+                return(False, "Amended film with invalid value for "+key+" reason was: "+reason)
+
+        
+    return(True, "Passed all tests")
+
 while True:
     ans = input("Test which module: database control (d), error checking (e) or both (b)")
     generate_values()
@@ -194,5 +310,7 @@ while True:
             print(g)
             if(g == 100):
                 print("Sucessfully ran 100 times")
-        
+    # Error checking module
+    elif(ans == "e"):
+        error_checking_test()
     
